ac2685658da1435c70c2a360d31d95428151ca7e
scimmiasaki <scimmiasaki_collassa@hotmail.com>
2016-03-14 13:43:05 +0100

    visualizza
    fa vedere i commenti ma non funziona il “for”

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 var root = 'http://jsonplaceholder.typicode.com';
 
 $(".btn").on('click',function(){
-//$.get('html/post.html', function ( callPost ) {
   $.ajax({
     url: root + '/posts',
     method: 'GET',
     contentType: 'application/json',
     dataType: 'json',
     success: function(show){
-  //    $('#gabbia').append(show);
-      console.log(show);
 
-      document.getElementById('gabbia').innerHTML = show
+
+  var len = show.length;
+  for (var i = 0; i < len; i++) {
+    //  document.getElementById('gabbia').innerHTML = (show[i].title);
+    document.getElementById('gabbia').innerHTML = (show[i].body);
+
+      }
 
     },
 
- });
-
-//  });
+  });
 
   $(".boxone").removeClass("appari");
- });
+
+});
